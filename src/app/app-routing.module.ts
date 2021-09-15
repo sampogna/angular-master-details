76d10c6bf755ba1f778ee186c2ddf773cc1ae1d6
User <sampogna.rafael@gmail.com>
2021-09-15 10:04:04 -0300

    adding navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesModule } from './pages/categories/categories.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' } Aparentemente descontinuado nas novas versoes do angular
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
